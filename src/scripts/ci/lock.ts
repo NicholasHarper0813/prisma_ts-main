111a02a381c0bb5a6685dbe0735e59b051085bb7
nicholas harper <nicholasharper000@gmail.com>
2024-12-24 16:59:29 -0800

    Update lock.ts

--- src/scripts/ci/lock.ts
+++ src/scripts/ci/lock.ts
@@ -7,13 +7,11 @@ const lock = promisify(require('redis-lock')(client))
 async function main() 
 {
   console.log('getting lock...')
-  console.log(new Date())
   const unlock = await lock('lockString2', 1000 * 1000)
   console.log('got lock')
   
   await new Promise(r => setTimeout(r, 1000 * 1000))
   console.log('released lock')
-  console.log(new Date())
   unlock()
 }
 
